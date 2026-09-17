6aac426a21f50323e0fb3c7ff05e258b0e217910
LeonAlbornoz <leonxxalbornoz20@gmail.com>
2026-09-17 01:32:47 -0300

    Add Scripts, Camera, NavAgent

--- Escenas/Player.gd
+++ Escenas/Player.gd
@@ -1,1 +1,28 @@
 extends CharacterBody2D
+
+@export var speed = 300
+
+@onready var anim: AnimatedSprite2D = $Anim
+
+var last_direction: String = "Down"
+#
+#func _ready() -> void:
+	#add_to_group("Player")
+
+func _physics_process(_delta: float) -> void:
+	var direction: Vector2 = Input.get_vector("Move_Left", "Move_Right", "Move_Up", "Move_Down")
+	
+	if direction != Vector2.ZERO:
+		velocity = direction * speed
+		
+		if abs(direction.x) > abs(direction.y):
+			last_direction = "Right" if direction.x > 0 else "Left"
+		else:
+			last_direction = "Down" if direction.y > 0 else "Up"
+		
+		anim.play("Walk_" + last_direction)
+	else:
+		velocity = Vector2.ZERO
+		anim.play("Idle_" + last_direction)
+	
+	move_and_slide()
--- Escenas/Potrillo.gd
+++ Escenas/Potrillo.gd
@@ -1,1 +1,28 @@
 extends CharacterBody2D
+
+@export var speed = 100
+
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("Player")
+
+#func _on_timer_timeout() -> void:
+	#var next_pos = nav_agent.
+	#nav_agent.
+
+func _ready() -> void:
+	nav_agent.path_desired_distance = 4.0
+	nav_agent.target_desired_distance = 10.0
+
+
+func _physics_process(_delta: float) -> void:
+	if player == null:
+		return
+	
+	nav_agent.target_position = player.global_position
+	
+	var next_pos := nav_agent.get_next_path_position()
+	var direction := global_position.direction_to(next_pos)
+	
+	velocity = direction * speed
+	
+	move_and_slide()
